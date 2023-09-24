31a54060a583d4eac64374801f3fea57f4bad56f
chiragfalor <cfalor@mit.edu>
2023-09-24 00:27:09 -0400

    multiple balls

--- examples/bounce/sketch.js
+++ examples/bounce/sketch.js
@@ -11,7 +11,44 @@ let ydirection = 1; // Top to Bottom
 //   return { x, y };
 // }
 
+let balls = [];
 
+class Ball {
+  constructor(x, y, r) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(random(-5, 5), random(-5, 5));
+    this.r = r;
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  update() {
+    // Update the position of the ball
+    this.pos.add(this.vel);
+
+    // Test to see if the ball exceeds the boundaries of the screen
+    // If it does, reverse its direction by multiplying by -1
+    if (this.pos.x > width - this.r || this.pos.x < this.r) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y > height - this.r || this.pos.y < this.r) {
+      this.vel.y *= -1;
+    }
+
+    // Check if the ball is outside of the triangle
+    if (!pointInTriangle(this.pos, v1, v2, v3)) {
+      const nearestEdge = getNearestEdge(this.pos, v1, v2, v3);
+      const normal = createVector(-nearestEdge.y, nearestEdge.x).normalize(); // normalize the nearestEdge vector
+      const dot = p5.Vector.dot(normal, this.vel);
+      const reflected = p5.Vector.sub(this.vel, p5.Vector.mult(normal, 2 * dot));
+      this.vel = reflected;
+    }
+  }
+
+  draw() {
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, this.r, this.r);
+  }
+}
 
 // Define the vertices of the triangle
 
@@ -21,8 +58,12 @@ function setup() {
   frameRate(30);
   ellipseMode(RADIUS);
   // Set the starting position of the shape
-  xpos = width / 2;
-  ypos = height / 2;
+  for (let i = 0; i < 10; i++) {
+    const x = random(width);
+    const y = random(height);
+    const r = 20;
+    balls.push(new Ball(x, y, r));
+  }
   
   v1 = createVector(100, 100);
   v2 = createVector(600, 100);
@@ -37,37 +78,16 @@ function draw() {
   fill(0, 255, 0);
   triangle(v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
 
-  // Update the position of the shape
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
-
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
-  if (xpos > width - rad || xpos < rad) {
-    xdirection *= -1;
+  for (const ball of balls) {
+    ball.update();
+    ball.draw();
   }
-  if (ypos > height - rad || ypos < rad) {
-    ydirection *= -1;
-  }
-
-  // Check if the shape is outside of the triangle
-
-  const p = createVector(xpos, ypos);
-  if (!pointInTriangle(p, v1, v2, v3)) {
-    const nearestEdge = getNearestEdge(p, v1, v2, v3);
-    const normal = createVector(-nearestEdge.y, nearestEdge.x).normalize(); // normalize the nearestEdge vector
-    const dot = p5.Vector.dot(normal, createVector(xspeed, yspeed));
-    const reflected = p5.Vector.sub(createVector(xspeed, yspeed), p5.Vector.mult(normal, 2 * dot));
-    // print(nearestEdge);
-    xspeed = reflected.x;
-    yspeed = reflected.y;
-  }
-  
-  fill(255, 0, 255);
-  ellipse(xpos, ypos, rad, rad);
-
-  // ellipse(xpos, ypos, rad, rad);
 }
+
+
+
+// utils
+
 
 function pointInTriangle(p, v1, v2, v3) {
   const b1 = sign(p, v1, v2) < 0;
