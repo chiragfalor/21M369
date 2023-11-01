dc1b530db6b4d94004ed1a4f95313f7d78898107
chiragfalor <cfalor@mit.edu>
2023-11-01 11:59:31 -0400

    minimal working example

--- Project 3 - Data Sonification/sketch.js
+++ Project 3 - Data Sonification/sketch.js
@@ -2,41 +2,45 @@
 // by Chirag Falor
 
 var img;
-var synth;
-var notes = ["C", "D", "E", "F", "G", "A", "B"];
-var octave = [0, 1, 2, 3, 4, 5, 6, 7];
+var osc;
 
 function preload() {
-    img = loadImage('yourImagePathHere.jpeg'); // Replace this with your image path
+    img = loadImage('juno_jupiter.jpg'); // Replace this with your image path
     synth = new p5.PolySynth();
 }
 
 function setup() {
-    createCanvas(256, 256);
-    image(img,0,0);
+  createCanvas(256, 256);
+  image(img,0,0);
+  img.loadPixels();
 
-    img.loadPixels();
-    frameRate(1);
+  osc = new p5.Oscillator();
+  osc.setType('sine');
+  osc.freq(440);
+  osc.amp(0.5);
+  osc.start();
+
+  frameRate(1);
 }
 
 function draw() {
-  var y = frameCount % img.height;
+  background(220);
+  var x = frameCount % img.width;
 
   // image highlighting
   image(img, 0, 0);
   fill(255, 0, 0, 50); // semi-transparent red
-  rect(y, 0, 1, img.height);
+  rect(x, 0, 1, height);
 
-  for (var x = 0; x < img.width; x++) {
+  for (var y = 0; y < img.height; y++) {
       var index = 4 * (x + y * img.width);
       var r = img.pixels[index];
       var g = img.pixels[index + 1];
       var b = img.pixels[index + 2];
       var bright = (r+g+b)/3; // brightness
-      var n = map(bright, 0, 255, 0, notes.length - 1);
-      var note = notes[Math.floor(n)];
-      var oct = octave[Math.floor(map(bright, 0, 255, 0, octave.length - 1))];
-      synth.play(note + oct, 0.1, 0, 1.0);
+      var freq = map(bright, 0, 255, 200, 800); // Mapping brightness to frequency
+      osc.freq(freq);
   }
 }
+
 
